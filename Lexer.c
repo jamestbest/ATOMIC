81e7eb337eb986353089c326a13a01c5f53cba43
James C <psyjc25@nottingham.ac.uk>
2023-11-08 20:17:37 +0000

    Started work on lexer
    Basic file io to get lines and then store their information

--- Lexer.c
+++ Lexer.c
@@ -2,7 +2,61 @@
 // Created by james on 28/10/23.
 //
 
-int test(int a) {
-    a++;
-    return a;
+#include "Lexer.h"
+
+Token* lex(FILE* file) {
+    /* Need to read in the file and turn the strings into tokens
+     * I don't want to read the whole file in
+     */
+
+    Token_vec tokens = Token_vec_create();
+
+    size_t buff_max = BUFF_SIZE;
+    /* Streaming tokens? not at the moment just store all the lines in a buffer and then store information on the position and size of a token */
+    charp_vec lines = charp_vec_create();
+
+    char* buff = malloc(buff_max * sizeof(char)); //store at least 100 characters
+
+    while (get_next_line(&buff, &buff_max, file)) {
+        //buff should contain the entire line as getline will resize the buffer to accommodate
+
+        char* line = malloc(buff_max);
+
+        if (line == NULL) {
+            perror("Error: Malloc call within lexing");
+            exit(errno);
+        }
+
+        memcpy(line, buff, buff_max);
+
+        charp_vec_add(&lines, line);
+
+        //[[maybe]] downsize the buffer to reduce the amount of memcpy otherwise a long line is going to increase the size of the buffer and all
+        //  subsequent runs will have to copy the larger buffer.
+    }
+
+    free(buff);
+
+    return 0;
+}
+
+
+
+size_t get_next_line(char** buff, size_t* buff_max, FILE* file) {
+    size_t ret;
+
+    ret = getline(buff, buff_max, file);
+
+    assert(errno != EINVAL);
+
+    if (errno == ENOMEM) {
+        printf("Error: OUT OF MEM L bozo");
+        exit(ENOMEM);
+    }
+
+    if (ret == -1) {
+        perror("Error: Failed to read new line in file");
+    }
+
+    return ret;
 }
--- Lexer.h
+++ Lexer.h
@@ -5,4 +5,70 @@
 #ifndef ATOMIC_LEXER_H
 #define ATOMIC_LEXER_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <error.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "SharedIncludes/Vector.c"
+
+#define BUFF_SIZE 100
+
+typedef enum TokenType {
+    IDENTIFIER,
+    KEYWORD,
+
+    LIT_STR,
+    LIT_INT,
+    LIT_FLOAT,
+    LIT_BOOL,
+    LIT_NAV,
+
+    OP_BIN,
+    OP_UN_PRE,
+    OP_UN_POST,
+    OP_TRINARY,
+
+    BRACKET_OPEN,
+    BRACKET_CLOSE,
+    CURLY_OPEN,
+    CURLY_CLOSE,
+    PAREN_OPEN,
+    PAREN_CLOSE,
+
+    COMMA,
+
+    WS_S,               //White space single/tab
+    WS_T,
+    NEWLINE,
+    EOTS,                //End of token stream
+} TokenType;
+
+/* Should the tokens store their location?
+ *   It would make printing their information easier
+ *
+ *   But it is not needed elsewhere in the compiler? Only really for printing the ast
+ *   Calculating the position of a single token would require looking at all previous tokens
+ *   But calculating the position of each token one by one sequentially would be fairly cheap
+ *   I would have to store the whitespace characters as well in order to calc the position instead of dropping '\t' and '\s'
+ *
+ *   [[maybe]] For now this will not store the positions
+ */
+typedef struct Token {
+    TokenType type;
+    char* line;
+    size_t size;
+    //a tokens value is the |func mainfunction () : i4|
+    //                            ^----------^
+    //                            |           `line + size
+    //                            `line
+} Token;
+
+VEC_ADD(Token, Token)
+
+size_t get_next_line(char** buff, size_t* buff_max, FILE* file);
+
 #endif //ATOMIC_LEXER_H
