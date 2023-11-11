114c22c6da7351c3865e393f5fd1f2b72541b3e6
jamestbest <james2004.coward@gmail.com>
2023-11-11 04:51:01 +0000

    New Flags.c files from preprocessor

--- Flags.c
+++ Flags.c
@@ -1,47 +1,15 @@
+// This is a string dawdad adwad wada D  Dwad awda dwadadwawadwadwsadwa
+// Created by jamescoward on 08/11/2023.
 //
-// Created by jamescoward on 04/11/2023.
-//
 
-#ifndef ATOMIC_FLAGS_H
-#define ATOMIC_FLAGS_H
-
-#include <stdbool.h>
-#include "SharedIncludes/helper.h"
-#include "SharedIncludes/FLAG_Shared.h"
-
-//flags
-//__FLAGS[__FLAG_00] = true;
-//`__` is reserved for start so `ATOM__` prefix
-/*
- * ATOM_FN__  --FUNCTION
- * ATOM_CT__  --CONSTANT
- * ATOM_VR__  --VARIABLE
- */
-
-//ATOM__FLAG_ is a prefix
-//THE NEXT 8 CHARACTERS ARE USED IN THE UNIQUE HASH
-//64 bit number - 8 characters in a row
-
-/*
- * Go through each flag defined and create a struct that stores the hash and the index?
- */
-
-/* [[NOTE]]
- * Must have the options enums and defines before the flags as they share the same internal buffer for the enums
- */
+#include "Flags.h"
 
 //%%OPTION ENUM%%
 enum ATOM_CT__OPTIONS {
     ATOM_CT__OPTION_E,
     ATOM_CT__OPTION_O,
     ATOM_CT__OPTION_OUT,
-    //%%END%%
 };
-
-//%%OPTION DEFINE%%
-#define ATOM_CT__OPTION_E_HASH 0x45
-#define ATOM_CT__OPTION_O_HASH 0x4f
-#define ATOM_CT__OPTION_OUT_HASH 0x54554f
 //%%END%%
 
 //%%FLAG ENUM%%
@@ -51,34 +19,16 @@ enum ATOM_CT__FLAGS {
     ATOM_CT__FLAG_TEST,
     ATOM_CT__FLAG_ANOTHER_FLAG,
     ATOM_CT__FLAG_TEST_FLAG,
-    ATOM_CT__FLAG_TEST_TWO_FLAG,
-    //%%END%%
+    ATOM_CT__FLAG_TEST_FOUR,
     ATOM_CT__FLAG_COUNT,
 };
-
-//%%FLAG DEFINE%% FLAG PREPROCESSOR GENERATED DEFINES SHOULD APPEAR HERE
-#define ATOM_CT__FLAG_TOK_OUT_HASH 0x54554f0d4b4f54
-#define ATOM_CT__FLAG_AST_OUT_HASH 0x54554f0d545341
-#define ATOM_CT__FLAG_TEST_HASH 0x54534554
-#define ATOM_CT__FLAG_ANOTHER_FLAG_HASH 0xd524548544f4e41
-#define ATOM_CT__FLAG_TEST_FLAG_HASH 0x414c460d54534554
-#define ATOM_CT__FLAG_TEST_TWO_FLAG_HASH 0x4f57540d54534554
-//%%END%%
-
-//%%FLAG STRINGS%%
-#define ATOM_CT__FLAG_TOK_OUT_STR "TOK_OUT"
-#define ATOM_CT__FLAG_AST_OUT_STR "AST_OUT"
-#define ATOM_CT__FLAG_TEST_STR "TEST"
-#define ATOM_CT__FLAG_ANOTHER_FLAG_STR "ANOTHER_FLAG"
-#define ATOM_CT__FLAG_TEST_FLAG_STR "TEST_FLAG"
-#define ATOM_CT__FLAG_TEST_TWO_FLAG_STR "TEST_TWO_FLAG"
 //%%END%%
 
 bool ATOM_VR__FLAGS[ATOM_CT__FLAG_COUNT];
 
 int flag_int_to_index(long long int fi) {
-    //%%FLAG INDEX SWITCH%% FLAG PREPROCESSOR GENERATED SWITCH CASE SHOULD APPEAR HERE
-	switch (fi) {
+    switch (fi) {
+        //%%FLAG INDEX SWITCH%% FLAG PREPROCESSOR GENERATED SWITCH CASE SHOULD APPEAR HERE
 		case ATOM_CT__FLAG_TOK_OUT_HASH:
 			return ATOM_CT__FLAG_TOK_OUT;
 		case ATOM_CT__FLAG_AST_OUT_HASH:
@@ -89,17 +39,17 @@ int flag_int_to_index(long long int fi) {
 			return ATOM_CT__FLAG_ANOTHER_FLAG;
 		case ATOM_CT__FLAG_TEST_FLAG_HASH:
 			return ATOM_CT__FLAG_TEST_FLAG;
-		case ATOM_CT__FLAG_TEST_TWO_FLAG_HASH:
-			return ATOM_CT__FLAG_TEST_TWO_FLAG;
+		case ATOM_CT__FLAG_TEST_FOUR_HASH:
+			return ATOM_CT__FLAG_TEST_FOUR;
 		default:
 			return -1;
-	}
-    //%%END%%
+        //%%END%%
+    }
 }
 
 char* flag_index_to_string(int index) {
-    //%%FLAG STR SWITCH%%
-	switch(index) {
+    switch(index) {
+        //%%FLAG STR SWITCH%%
 		case ATOM_CT__FLAG_TOK_OUT:
 			return ATOM_CT__FLAG_TOK_OUT_STR;
 		case ATOM_CT__FLAG_AST_OUT:
@@ -110,12 +60,12 @@ char* flag_index_to_string(int index) {
 			return ATOM_CT__FLAG_ANOTHER_FLAG_STR;
 		case ATOM_CT__FLAG_TEST_FLAG:
 			return ATOM_CT__FLAG_TEST_FLAG_STR;
-		case ATOM_CT__FLAG_TEST_TWO_FLAG:
-			return ATOM_CT__FLAG_TEST_TWO_FLAG_STR;
+		case ATOM_CT__FLAG_TEST_FOUR:
+			return ATOM_CT__FLAG_TEST_FOUR_STR;
 		default:
 			return "ERROR NO ENUM NAME";
-	}
-    //%%END%%
+        //%%END%%
+    }
 }
 
 int flag_to_index(char* flag) {
@@ -144,5 +94,3 @@ void print_flags() {
     }
     printf("`-%s : %s" C_RST "\n", flag_index_to_string(ATOM_CT__FLAG_COUNT - 1), flag_get(ATOM_CT__FLAG_COUNT - 1) == 1 ? C_GRN "True" : C_RED "False");
 }
-
-#endif //ATOMIC_FLAGS_H
---
+++ Flags.h
@@ -0,0 +1,71 @@
+//
+// Created by jamescoward on 04/11/2023.
+//
+
+#ifndef ATOMIC_FLAGS_H
+#define ATOMIC_FLAGS_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include "SharedIncludes/Helper_Math.h"
+#include "SharedIncludes/Flag_shared.h"
+#include "SharedIncludes/Colours.h"
+
+//flags
+//__FLAGS[__FLAG_00] = true;
+//`__` is reserved for start so `ATOM_XX__` prefix
+/*
+ * ATOM_FN__  --FUNCTION
+ * ATOM_CT__  --CONSTANT
+ * ATOM_VR__  --VARIABLE
+ */
+
+//ATOM__FLAG_ is a prefix
+//THE NEXT 8 CHARACTERS ARE USED IN THE UNIQUE HASH
+//64 bit number - 8 characters in a row
+
+/*
+ * Go through each flag defined and create a struct that stores the hash and the index?
+ */
+
+/* [[NOTE]]
+ * Must have the options enums and defines before the flags as they share the same internal buffer for the enums
+ */
+
+enum ATOM_CT__OPTIONS;
+enum ATOM_CT__FLAGS;
+
+//%%OPTION DEFINE%%
+#define ATOM_CT__OPTION_E_HASH 0x45
+#define ATOM_CT__OPTION_O_HASH 0x4f
+#define ATOM_CT__OPTION_OUT_HASH 0x54554f
+//%%END%%
+
+//%%FLAG DEFINE%% FLAG PREPROCESSOR GENERATED DEFINES SHOULD APPEAR HERE
+#define ATOM_CT__FLAG_TOK_OUT_HASH 0x54554f5f4b4f54
+#define ATOM_CT__FLAG_AST_OUT_HASH 0x54554f5f545341
+#define ATOM_CT__FLAG_TEST_HASH 0x54534554
+#define ATOM_CT__FLAG_ANOTHER_FLAG_HASH 0x5f524548544f4e41
+#define ATOM_CT__FLAG_TEST_FLAG_HASH 0x414c465f54534554
+#define ATOM_CT__FLAG_TEST_FOUR_HASH 0x554f465f54534554
+//%%END%%
+
+//%%FLAG STRINGS%%
+#define ATOM_CT__FLAG_TOK_OUT_STR "TOK_OUT"
+#define ATOM_CT__FLAG_AST_OUT_STR "AST_OUT"
+#define ATOM_CT__FLAG_TEST_STR "TEST"
+#define ATOM_CT__FLAG_ANOTHER_FLAG_STR "ANOTHER_FLAG"
+#define ATOM_CT__FLAG_TEST_FLAG_STR "TEST_FLAG"
+#define ATOM_CT__FLAG_TEST_FOUR_STR "TEST_FOUR"
+//%%END%%
+
+int flag_int_to_index(long long int fi);
+char* flag_index_to_string(int index);
+
+void print_flags();
+bool flag_get(enum ATOM_CT__FLAGS flag);
+bool flag_set(char* flagName, bool enable);
+int flag_to_index(char* flag);
+
+
+#endif //ATOMIC_FLAGS_H
