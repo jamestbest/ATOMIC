6d76150bd3189aa2c72b8e95e3793259f678f074
jamestbest <james2004.coward@gmail.com>
2023-11-12 03:27:07 +0000

    Added Compiler.c/h for base level interface

---
+++ Compiler.c
@@ -0,0 +1,10 @@
+//
+// Created by jamescoward on 11/11/2023.
+//
+
+#include "Compiler.h"
+
+void compile(const char* entry_point, const char* out_format) {
+
+    return;
+}
---
+++ Compiler.h
@@ -0,0 +1,10 @@
+//
+// Created by jamescoward on 11/11/2023.
+//
+
+#ifndef ATOMIC_COMPILER_H
+#define ATOMIC_COMPILER_H
+
+void compile(const char* entry_point, const char* out_format);
+
+#endif //ATOMIC_COMPILER_H
