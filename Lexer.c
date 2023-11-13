d68b9934c250014e8c6eee0553ff4ea006755fd9
jamestbest <james2004.coward@gmail.com>
2023-11-13 00:45:12 +0000

    Updated Lexer.c/h to change the format of the base lex function to take in lines and tokens so that they can be freed by calling compile function

--- Lexer.c
+++ Lexer.c
@@ -4,30 +4,22 @@
 
 #include "Lexer.h"
 
-VEC_ADD(Token, Token)
-
-Token_vec lex(FILE* file) {
+void lex(FILE* file, Token_vec* tokens, charp_vec* lines) {
     /*
      * Need to read in the file and turn the strings into tokens
-     * I don't want to read the whole file in
      */
-    Token_vec tokens = Token_vec_create(BUFF_SIZE);
 
     /* Streaming tokens? not at the moment just store all the lines in a buffer and then store information on the position and size of a token */
-    charp_vec lines = charp_vec_create(BUFF_SIZE);
-
     char_vec buffer = char_vec_create(BUFF_SIZE);
 
     while (get_line(file, &buffer)) {
-        line_to_tokens(&buffer, &tokens);
+        line_to_tokens(&buffer, tokens);
 
         char* line = char_vec_steal(&buffer);
-        charp_vec_add(&lines, line);
+        charp_vec_add(lines, line);
     }
 
     char_vec_destroy(&buffer);
-
-    return tokens;
 }
 
 
--- Lexer.h
+++ Lexer.h
@@ -16,60 +16,11 @@
 #include "SharedIncludes/Vector.h"
 #include "SharedIncludes/Helper_File.h"
 
+#include "Tokens.h"
+
 #define BUFF_SIZE 100
 
-typedef enum TokenType {
-    IDENTIFIER,
-    KEYWORD,
-
-    LIT_STR,
-    LIT_INT,
-    LIT_FLOAT,
-    LIT_BOOL,
-    LIT_NAV,
-
-    OP_BIN,
-    OP_UN_PRE,
-    OP_UN_POST,
-    OP_TRINARY,
-
-    BRACKET_OPEN,
-    BRACKET_CLOSE,
-    CURLY_OPEN,
-    CURLY_CLOSE,
-    PAREN_OPEN,
-    PAREN_CLOSE,
-
-    COMMA,
-
-    WS_S,                   //White space single/tab
-    WS_T,
-    NEWLINE,
-    EOTS,                   //End of token stream
-} TokenType;
-
-/* Should the tokens store their location?
- *   It would make printing their information easier
- *
- *   But it is not needed elsewhere in the compiler? Only really for printing the ast    ----AND ERRORS!
- *   Calculating the position of a single token would require looking at all previous tokens
- *   But calculating the position of each token one by one sequentially would be fairly cheap
- *   I would have to store the whitespace characters as well in order to calc the position instead of dropping '\t' and '\s'
- *
- *   [[maybe]] For now this will not store the positions
- */
-//a tokens value is the |func mainfunction () : i4|
-//                            ^----------^
-//                            |           `line + size
-//                            `line
-typedef struct Token {
-    TokenType type;
-    char* start;
-    size_t size;
-} Token;
-
-VEC_PROTO(Token, Token)
-
+void lex(FILE* file, Token_vec* tokens, charp_vec* lines);
 void line_to_tokens(char_vec* line, Token_vec* tokens);
 
 #endif //ATOMIC_LEXER_H
