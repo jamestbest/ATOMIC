14d554a1946d6a4210556448b2fe845aeecfe072
jamestbest <james2004.coward@gmail.com>
2023-11-13 00:42:12 +0000

    Updated CLI.c/h with new Compiler.c function format

--- CLI.c
+++ CLI.c
@@ -32,8 +32,6 @@ int main(int argc, char** argv) {
     printf("Entry point: %s\n"
            "Output format: %s\n", ATOM_VR__CLI_ENTRY_POINT, ATOM_VR__CLI_OUTPUT_NAME);
 
-    compile(ATOM_VR__CLI_ENTRY_POINT, ATOM_VR__CLI_OUTPUT_NAME);
-
     putchar('\n');
     for (uint i = 0; i < ATOM_VR__CLI_FILES.pos; i++) {
         char* filename = charp_vec_get(&ATOM_VR__CLI_FILES, i);
@@ -41,17 +39,36 @@ int main(int argc, char** argv) {
         printf("File: %s\n", filename);
     }
 
-    return SUCCESS;
+    CompileRet ret = compile(ATOM_VR__CLI_ENTRY_POINT, ATOM_VR__CLI_OUTPUT_NAME, ATOM_VR__CLI_FILES);
+
+    putchar('\n');
+    parse_compile_ret(ret);
+
+    return ret.code;
+}
+
+void parse_compile_ret(CompileRet ret) {
+    switch (ret.code) {
+        case SUCCESS:
+            printf(ATOM_CT__CLI_SUCC);
+            break;
+        case ERR_NO_SUCH_FILE:
+            printf(ATOM_CT__CLI_ERR_NO_SUCH_FILE, ret.info);
+            break;
+        default:
+            printf(ATOM_CT__CLI_ERR_UNKNOWN, ret.code);
+            break;
+    }
 }
 
 bool verify_args(int argc, char** argv) {
     if (argc <= 1) {
-        puts(ATOM_CT__CLI_ERR_ARG_NONE);
+        PError(ATOM_CT__CLI_ERR_ARG_NONE);
         return false;
     }
 
     if (argv[1] == 0) {
-        puts(ATOM_CT__CLI_ERR_ARG_NULL);
+        PError(ATOM_CT__CLI_ERR_ARG_NULL);
         return false;
     }
 
@@ -83,7 +100,7 @@ void parse_args(int argc, char** argv) {
             if (arg_len >= 2 && arg[1] == '-') {
                 parse_flag(arg); //--flagSet  --!flagset
             } else {
-                parse_option(arg, arg_len, argv, argc, &i); //-o optionarg optionarg   e.g. -o tok ast  --this can map to flags.  -outname main.out --changes output name
+                parse_option(arg, argv, argc, &i); //-o optionarg optionarg   e.g. -o tok ast  --this can map to flags.  -outname main.out --changes output name
             }
         } else {
             parse_file(arg); // inputfile.atom/inputfile.atm
@@ -96,13 +113,13 @@ void parse_file(char* file) {
 }
 
 void parse_option_entry(charp_vec* args) {
-    if (args->pos != 1) printf(ATOM_CT__CLI_WRN_OPT_ARG_COUNT, ATOM_CT__OPTION_E_STR);
+    if (args->pos != 1) PWarn(ATOM_CT__CLI_WRN_OPT_ARG_COUNT, ATOM_CT__OPTION_E_STR);
 
     ATOM_VR__CLI_ENTRY_POINT = charp_vec_get(args, 0);
 }
 
 void parse_option_output(charp_vec* args) {
-    if (args->pos != 1) printf(ATOM_CT__CLI_WRN_OPT_ARG_COUNT, ATOM_CT__OPTION_O_STR);
+    if (args->pos != 1) PWarn(ATOM_CT__CLI_WRN_OPT_ARG_COUNT, ATOM_CT__OPTION_O_STR);
 
     ATOM_VR__CLI_OUTPUT_NAME = charp_vec_get(args, 0);
 }
@@ -118,7 +135,7 @@ void parse_option_out(charp_vec* args) {
         int index = flag_int_to_index(hash);
 
         if (index == -1) {
-            printf(ATOM_CT__CLI_WRN_OPT_ARG_INVLD, ATOM_CT__OPTION_OUT_STR, arg);
+            PWarn(ATOM_CT__CLI_WRN_OPT_ARG_INVLD, ATOM_CT__OPTION_OUT_STR, arg);
             continue;
         }
 
@@ -129,13 +146,13 @@ void parse_option_out(charp_vec* args) {
 /*
  * Parse the option and use the argv values as arguments
  */
-void parse_option(char* arg, uint arg_len, char** argv, int argc, int* i) {
+void parse_option(char *arg, char **argv, int argc, int *i) {
     llint option_hash = flag_to_int(&arg[1]);
 
     charp_vec args = get_option_args(argv, i, argc);
 
     if (args.pos == 0) {
-        printf(ATOM_CT__CLI_ERR_OPT_ARG, arg);
+        PError(ATOM_CT__CLI_ERR_OPT_ARG, arg);
         exit(ARGERR);
     }
 
@@ -182,7 +199,7 @@ void parse_flag(char* arg) {
     bool ret = flag_set(&arg[start], enabled);
 
     if (!ret) {
-        printf(ATOM_CT__CLI_WRN_OPT_FLG_INVLD, &arg[start]);
+        PWarn(ATOM_CT__CLI_WRN_OPT_FLG_INVLD, &arg[start]);
         return;
     }
 }
--- CLI.h
+++ CLI.h
@@ -23,12 +23,18 @@ typedef long long int llint;
 #define ATOM_CT__CLI_DEFAULT_OUT                "out.a"
 #define ATOM_CT__CLI_DEFAULT_FILE_BUFF_SIZE     4
 
+#define ATOM_CT__CLI_SUCC                       SUCC_BASE"Compilation complete!\n"
+#define ATOM_CT__CLI_ERR_NO_SUCH_FILE           ERROR_BASE"No such file found: %s\n"
+#define ATOM_CT__CLI_ERR_UNKNOWN                ERROR_BASE"Unknown error code %d\n"
+
 bool verify_args(int argc, char** argv);
 void parse_args(int argc, char** argv);
 
 void parse_flag(char* arg);
-void parse_option(char* arg, uint arg_len, char** argv, int argc, int* i);
+void parse_option(char *arg, char **argv, int argc, int *i);
 charp_vec get_option_args(char** argv, int* argp, int argc);
 void parse_file(char* file);
+
+void parse_compile_ret(CompileRet ret);
 
 #endif //ATOMIC_CLI_H
