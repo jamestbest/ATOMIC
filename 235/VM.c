5fded54e6a1528a8e569dd55caa96efffc6de8d1
jamestbest <james2004.coward@gmail.com>
2025-11-03 16:44:59 +0000

    Added assignment expressions to the VM

--- 235/VM.c
+++ 235/VM.c
@@ -2,14 +2,17 @@
 // Created by jamescoward on 10/09/2025.
 //
 
-#include <assert.h>
 #include "VM.h"
+
+#include "Kall.h"
+
+#include <assert.h>
 
 bool running= true;
 size_t ip= 0;
 
 // 1 MB of stack space
-#define STACK_SIZE 1024 * 1024 * 1
+#define STACK_SIZE (1024 * 1024 * 1)
 
 uint64_t registers[MAX_REG + 1]= {0};
 uint8_t stack[STACK_SIZE]= {0};
@@ -33,7 +36,8 @@ uint64_t value_to_u64(Value v){
                 return bp[v.data.reg - ARG_REG_S + 1];
             }
             if (v.data.reg <= MAX_REG) return registers[v.data.reg];
-            else assert(false);
+
+            assert(false);
         case VT_IMM_I:
             return v.data.i64;
         case VT_IMM_U:
@@ -45,27 +49,38 @@ uint64_t value_to_u64(Value v){
     assert(false);
 }
 
+uint64_t size_down(uint8_t size, uint64_t value) {
+    switch (size) {
+        case 1:
+            value= (uint8_t)value;
+            break;
+        case 2:
+            value= (uint16_t)value;
+            break;
+        case 4:
+            value= (uint32_t)value;
+            break;
+        case 8:
+            value= (uint64_t)value;
+            break;
+    }
+    return value;
+}
+
+void assign(uint8_t size, Value r, uint8_t res_reg) {
+    uint64_t rv= value_to_u64(r);
+    uint8_t dest= res_reg;
+
+    registers[dest]= size_down(size, rv);
+}
+
 void add(uint8_t size, Value l, Value r, uint8_t res_reg) {
     uint64_t lv= value_to_u64(l);
     uint64_t rv= value_to_u64(r);
 
     uint64_t res= lv + rv;
 
-    switch (size) {
-        case 1:
-            res= (uint8_t)res;
-            break;
-        case 2:
-            res= (uint16_t)res;
-            break;
-        case 4:
-            res= (uint32_t)res;
-            break;
-        case 8:
-            break;
-        default:
-            assert(false);
-    }
+    res= size_down(size, res);
 
     registers[res_reg]= res;
 }
@@ -74,16 +89,19 @@ void run_arith_bi(ByteStmt* stmt) {
     Value l= Value_arr_get(&stmt->args, 0);
     Value r= Value_arr_get(&stmt->args, 1);
 
-    switch (stmt->arith_idx) {
-        case PLUS:
+    switch (stmt->sub_idx) {
+        case OP_PLUS:
             add(stmt->size, l, r, stmt->dest_reg);
+            break;
+        case OP_ASSIGN:
+            assign(stmt->size, r, l.data.reg);
             break;
         default:
             assert(false);
     }
 }
 
-void create_frame(size_t ret, ValueArray* args) {
+void create_frame(const size_t ret, const ValueArray* args) {
     *sp= (uint64_t)bp;
     sp++;
 
@@ -94,7 +112,7 @@ void create_frame(size_t ret, ValueArray* args) {
     for (size_t i= 0; i < args->pos; ++i) {
         Value v= Value_arr_get(args, i);
 
-        (*sp)= value_to_u64(v);
+        *sp= value_to_u64(v);
         sp++;
     }
 
@@ -163,6 +181,10 @@ int run(ByteStmtArray stmts, StringEntryArray functions) {
             case B_ARITH_BI:
                 run_arith_bi(stmt);
                 break;
+            case B_KALL: {
+                kall(stmt->sub_idx, stmt->args);
+                break;
+            }
         }
 
         ip++;
